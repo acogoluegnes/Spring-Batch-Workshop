04731558b90c4f90b2120259ed7eb7ed3bbd1e41
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2011-07-28 15:40:13 +0200

    added XML file reading sample

---
+++ xml-file-reading-solution/src/main/resources/create-tables.sql
@@ -0,0 +1,8 @@
+create table contact(
+	id int,
+	firstname varchar(255),
+	lastname varchar(255),
+	birth date
+);
+
+create sequence contact_seq;
---
+++ xml-file-reading-start/src/main/java/com/zenika/workshop/springbatch/Contact.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.zenika.workshop.springbatch;
+
+import java.util.Date;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class Contact {
+
+	private Long id;
+	
+	private String firstname,lastname;
+	
+	private Date birth;
+
+	public Contact(String firstname, String lastname, Date birth) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.birth = birth;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", firstname=" + firstname + ", lastname="
+				+ lastname + ", birth=" + birth + "]";
+	}
+	
+}
